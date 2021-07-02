4d72603cccf37cdff5720988e20a58ea42f7227e
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2021-07-01 22:07:38 -0500

    Unos ajustes.

--- code/talleres/javascript-advanced/swapi/promesas_encadenadas.js
+++ code/talleres/javascript-advanced/swapi/promesas_encadenadas.js
@@ -3,28 +3,26 @@ const fetchData = require("../utils/fetchData")
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
 
-let url = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
+const obtenerPersonaje = function(id) {
+    return `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+}
 
-fetchData( url )
+fetchData( obtenerPersonaje(1) )
     .then( (data) => {
         console.log( data.name )
-        url = `${API_URL}${PEOPLE_URL.replace(':id', 2)}`
-        return fetchData(url)
+        return fetchData(obtenerPersonaje(2))
     } )
     .then( (data) => {
         console.log( data.name )
-        url = `${API_URL}${PEOPLE_URL.replace(':id', 3)}`
-        return fetchData(url)
+        return fetchData(obtenerPersonaje(3))
     } )
     .then( (data) => {
         console.log( data.name )
-        url = `${API_URL}${PEOPLE_URL.replace(':id', 4)}`
-        return fetchData(url)
+        return fetchData(obtenerPersonaje(4))
     } )
     .then( (data) => {
         console.log( data.name )
-        url = `${API_URL}${PEOPLE_URL.replace(':id', 5)}`
-        return fetchData(url)
+        return fetchData(obtenerPersonaje(5))
     } )
     .then( (data) => {
         console.log( data.name )
